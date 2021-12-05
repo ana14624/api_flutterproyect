324916c7dc3d69d771b31ad6bda54ae1eeda4a6f
ana14624 <anayeligonzalez14624@gmail.com>
2021-12-04 19:12:24 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const mysql= require('mysql');
 
 const mysqlConnection =  mysql.createConnection({
-    host: 'sql5.freemysqlhosting.net',
-    user:'sql5449581',
+    host: 'sql3.freemysqlhosting.net',
+    user:'sql3456444',
     port: '3306',
-    password: 'teLBwm5eRN',
-    database: 'sql5449581'
+    password: 'gmjcPFuP95',
+    database: 'sql3456444'
 });
 
 mysqlConnection.connect(function (err) {
